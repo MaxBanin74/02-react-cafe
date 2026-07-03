b674d8cde52799997a321be1f392ebdcfae14cfd
Max Banin <banin@ukr.net>
2026-07-03 16:26:18 +0300

    2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import css from "./App.module.css";
+import type { Votes, VoteType } from "../../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  function handleVote(type: VoteType) {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  }
+
+  function resetVote() {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  }
+
+  const totalVotes: number = votes.good + votes.neutral + votes.bad;
+  let positiveRate =
+    totalVotes > 0
+      ? (positiveRate = Math.round((votes.good * 100) / totalVotes))
+      : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVote}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
